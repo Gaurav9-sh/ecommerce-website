bc8160c0ab24edb7d09fa113e14ab84cbe1bd9b4
Gaurav Sharma <gauravsh650@gmail.com>
2023-12-16 17:57:19 +0530

    adding sales section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './Card.css'
+import Star from '../images/Star.jpg'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='card'>
         <div className="cardImage">
-            
+            <img src={props.image}  alt="" />
         </div>
+        <h4 style={{fontFamily:"poppins", fontSize:"1rem", fontWeight:"600", marginTop:"1rem"}}>{props.title}</h4>
+        <div className="specification">
+        <p style={{color:"#DB4444", fontFamily:"poppins"}}>${props.price}</p> <p style={{fontFamily:"poppins", color:"grey"}}>${props.price2}</p>
+        </div>
+        <div className="starContainer" style={{marginTop:"-1rem"}}>
+          <img src={Star}  style = {{height: "1rem", width: "1rem"}} alt="" />
+          <img src={Star}  style = {{height: "1rem", width: "1rem"}} alt="" />
+          <img src={Star}  style = {{height: "1rem", width: "1rem"}} alt="" />
+          <img src={Star}  style = {{height: "1rem", width: "1rem"}} alt="" />
+          <img src={Star}  style = {{height: "1rem", width: "1rem"}} alt="" />
+        </div>
+        
+       
     </div>
   )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import TopCarousel from './TopCarousel'
 import Sidebar from './Sidebar'
 import SalesSection from './SalesSection'
+import Category from './Category'
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
         <TopCarousel/>
         <Sidebar/>
         <SalesSection/>
+        <Category/>
     </div>
   )
 }
